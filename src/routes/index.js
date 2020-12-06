79ee62b84daa464e264928aac0872460d6b62872
svana00 <54863023+svana00@users.noreply.github.com>
2020-12-06 18:13:33 +0000

    blank main w routes

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from '../views/Main';
+
+export default createAppContainer(createStackNavigator({
+  Main,
+}));
---
+++ src/views/Main/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hello: false,
+    };
+  }
+
+  render() {
+    const {
+      hello,
+    } = this.state;
+    return (
+      <View style={{ flex: 1, backgroundColor: '#e5e5e5' }}>
+        <Text>{hello}</Text>
+      </View>
+    );
+  }
+}
+Main.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+    addListener: PropTypes.func.isRequired,
+  }).isRequired,
+};
+export default Main;
