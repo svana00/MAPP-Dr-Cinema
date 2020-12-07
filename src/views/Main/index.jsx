6f3a9fac3e4605e9cf338fac1dd891a8dab73bd3
m4gga <margriette123@gmail.com>
2020-12-07 14:56:16 +0000

    ogedslegt en tveir takkar lol

--- src/views/Main/index.jsx
+++ src/views/Main/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import PropTypes from 'prop-types';
+import { Button } from 'react-native-paper';
 
 class Main extends React.Component {
   constructor(props) {
@@ -17,6 +18,19 @@ class Main extends React.Component {
     return (
       <View style={{ flex: 1, backgroundColor: '#e5e5e5' }}>
         <Text>{hello}</Text>
+        <Text>Hello there!</Text>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{ flex: 1 }}>
+            <Button mode="contained" color="black" compact="True" onPress={() => console.log('Cinemas')}>
+              <Text>Cinemas</Text>
+            </Button>
+          </View>
+          <View style={{ flex: 1, width: 20 }}>
+            <Button mode="contained" color="black" compact="True" onPress={() => console.log('UpcomingMovies')}>
+              <Text>Upcoming Movies</Text>
+            </Button>
+          </View>
+        </View>
       </View>
     );
   }
