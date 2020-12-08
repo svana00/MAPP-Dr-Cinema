59e750065761b020324a7ee526361069c56a4bb7
Róslín Erla <roslin19@ru.is>
2020-12-08 19:11:05 +0000

    token service should be ready

--- src/actions/cinemaMoviesActions.js
+++ src/actions/cinemaMoviesActions.js
@@ -1,5 +1,5 @@
 import * as constants from '../constants';
-import { getAllMoviesForCinema } from '../services/cinemasService';
+import { getAllMoviesForCinema, token } from '../services/cinemasService';
 
 export const getMoviesForCinemaSuccess = (movies) => ({
   type: constants.GET_CINEMA_MOVIES,
@@ -8,10 +8,11 @@ export const getMoviesForCinemaSuccess = (movies) => ({
 
 export const getMoviesForCinema = (cinemaId) => async (dispatch) => {
   try {
-    const movies = await getAllMoviesForCinema(cinemaId).getMovies();
-    console.log(movies);
+    var finalToken = await token();
+    finalToken = finalToken.token;
+    const movies = await getAllMoviesForCinema(cinemaId, finalToken).getMovies();
     dispatch(getMoviesForCinemaSuccess(movies));
   } catch (err) {
-    console.log('We had an ERROR in movieAction --> getCinemaDetails');
+    console.log('We had an ERROR in movieAction --> getCinemaDetails', err);
   }
 };
--- src/services/cinemasService.js
+++ src/services/cinemasService.js
@@ -1,16 +1,35 @@
 import fetch from 'node-fetch';
 
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1ZmNlMGRkNTE2ZjczZjE2NjQ4OThjNTkiLCJnbG9iYWxhZG1pbiI6ZmFsc2UsImFkbWluIjpmYWxzZSwiYWN0aXZlIjp0cnVlLCJmdWxsbmFtZSI6IlLDs3Nsw61uIEVybGEgVMOzbWFzZMOzdHRpciIsImVtYWlsIjoicm9zbGluZXJsYTI2MDlAZ21haWwuY29tIiwidXNlcm5hbWUiOiJyb3NldGhlZmwwd2VyIiwicGFzc3dvcmQiOiIkMmEkMDgkc2c4QS9UdFlmaEFhdnBINC45ems0LkZ3N2RFYlh5TXc0RC5jejJYNkxYMVZyNUVIMnozREciLCJkb21haW4iOiJsb2NhbGhvc3QiLCJtZXNzYWdlIjoiTGVhcm5pbmcgaG93IHRvIG1ha2UgYW4gYXBwIGluIHNjaG9vbCB0aGF0IHJlbGllcyBvbiBpbmZvIGZyb20gYW4gZXh0ZXJuYWwgYXBpIiwiaWF0IjoxNjA3NDQzMjgwLCJleHAiOjE2MDc1Mjk2ODB9.Laq2EQcFOR_6x0iEsM8RAzKwfDbGprZB_RLa3go2QcU';
+const AUTHENTICATIONENDPOINT = 'https://api.kvikmyndir.is/authenticate'
 const CINEMASENDPOINT = 'http://api.kvikmyndir.is/theaters';
 const MOVIEENDPOINT = 'http://api.kvikmyndir.is/movies';
 
-export function getAllCinemas() {
+
+export const token = () => {
+  var payload = {username : "RoseTheFl0wer",
+                 password : "smaforritmarosv"}
+  return fetch(AUTHENTICATIONENDPOINT, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: {
+    "Content-type": "application/json; charset=UTF-8"}
+  }).then(async (response) => {
+    if (response.status >= 200 && response.status < 300) {
+      return response.json();
+    }
+    throw new Error('did not work ', response);
+  }).catch((error) => { console.log(error); });
+}
+
+export async function getAllCinemas() {
+  var finalToken = await token()
+  finalToken = finalToken.token
   return fetch(CINEMASENDPOINT, {
     method: 'GET',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
-      'x-access-token': token,
+      'x-access-token': finalToken,
       'Accept-Charset': 'utf-8',
     },
   }).then((response) => {
@@ -18,14 +37,14 @@ export function getAllCinemas() {
       return response.json();
     }
     throw new Error('error ', response);
-  }).catch((error) => { console.log(error); });
+  }).catch((error) => { console.log("THIS ERROR", error); });
 }
 
-export const getAllMoviesForCinema = (cinemaId) => ({
+export const getAllMoviesForCinema = (cinemaId, finalToken) => ({
   getMovies: () => fetch(MOVIEENDPOINT, {
     method: 'GET',
     headers: {
-      'x-access-token': token,
+      'x-access-token': finalToken,
     },
   })
     .then((d) => d.json())
@@ -51,6 +70,7 @@ export const getAllMoviesForCinema = (cinemaId) => ({
       return temp;
     }),
 });
+
 
 const GetGenres = (genreArray) => {
   let genreStr = '';
