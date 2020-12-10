561c294ee614e18e671c63fe7ef86eab55f409a3
svana00 <54863023+svana00@users.noreply.github.com>
2020-12-10 20:11:34 +0000

    minor error fix

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import cinemas from './cinemasReducer';
 import cinemaMovies from './cinemaMoviesReducer';
 import upcomingMovies from './upcomingMoviesReducer';
-import allMovies from './allMoviesReducer'
+import allMovies from './allMoviesReducer';
 
 export default combineReducers({
   cinemas,
